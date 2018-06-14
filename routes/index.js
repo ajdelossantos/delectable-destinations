1a938cace1ddecb3b33f85b6b07417fd5652f466
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-06-13 21:33:04 -0700

    Annotate index.js and app.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 // Do work here
+// req has the info; res has methods for sending data back
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log("\n========================================\nRequest made to '/'\n========================================\n");
+  res.send('<h2 id="success">Hey! It works!</h2>');
+
+  // Will echo all query params
+  // res.json(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reversedName = [...req.params.name].reverse().join('');
+  res.send(reversedName);
+})
 
 module.exports = router;
