d421a4a72695627658d0620010cbe01fe5878be7
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-06-29 14:08:18 -0700

    Revise routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,26 @@ const router = express.Router();
 // req has the info; res has methods for sending data back
 router.get('/', (req, res) => {
   console.log("\n========================================\nRequest made to '/'\n========================================\n");
-  res.send('<h2 id="success">Hey! It works!</h2>');
+  // res.send('<h2 id="success">Hey! It works!</h2>');
 
   // Will echo all query params
   // res.json(req.query);
+
+  res.render('test', {
+    title: 'Test',
+    name: 'Alvin',
+    dog: 'Archie'
+  });
+
 });
 
-router.get('/reverse/:name', (req, res) => {
-  const reversedName = [...req.params.name].reverse().join('');
-  res.send(reversedName);
-})
+/*
+  // At this route...
+  router.get('/reverse/:name', (req, res) => {
+    // inner function hadles param and output
+    const reversedName = [...req.params.name].reverse().join('');
+    res.send(reversedName);
+  })
+*/
 
 module.exports = router;
