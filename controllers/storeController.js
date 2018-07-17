c00ba7f912a6926fbc50b38dd4da6fb99ca49991
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-07-16 22:54:07 -0700

    Finish editStore.pug mixin

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,5 +4,14 @@ exports.homePage = (req, res) => {
   console.log(
     "\n========================================\nRequest made to '/'\n========================================\n"
   );
-  res.render("index");
+  res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 };
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const storeController = require('../controllers/storeController');
 // req has the info; res has methods for sending data back
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
