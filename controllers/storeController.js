a79daedeb74086b8cfe0099ea7f1bfe68f2c915c
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-07-17 17:51:59 -0700

    Add updateStore to storeController

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -33,3 +33,28 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+  // 1. Find store with ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm user is owner
+  // TODO when AUTH done
+  // 3. Render out edit form
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+
+  req.flash(
+    'success',
+    `Successfully updated <stromg>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store!</a>`
+  );
+
+  res.redirect(`/stores/${store._id}/edit`);
+};
