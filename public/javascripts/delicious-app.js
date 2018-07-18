eac68cc9362e720223d258f556796e33202adbc6
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-07-17 21:15:59 -0700

    Implement address autocomplete via Google Maps API

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+// Takes Lat, then Lng 'the right way'
+autocomplete($('#address'), $('#lat'), $('#lng'));
---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,20 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; // short-circuit out of no input
+
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    console.log(place);
+
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+
+  // Prevent default behavior of enter key
+  input.on('keydown', e => {
+    if (e.keyCode === 13) e.preventDefault();
+  });
+}
+
+export default autocomplete;
